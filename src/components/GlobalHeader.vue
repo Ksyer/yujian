afb2a53f51da095213594657173e507f4cb980dc
huatree <wang.yaohua@foxmail.com>
2021-05-09 12:41:31 +0800

    🌈 style(顶部导航): 调整样式在标签中的位置

--- src/components/GlobalHeader.vue
+++ src/components/GlobalHeader.vue
@@ -1,17 +1,17 @@
 <template>
   <nav class="navbar navbar-dark bg-primary justify-content-between mb-4 px-4">
     <a class="navbar-brand" href="#">抒见专栏</a>
-    <ul v-if="!user.isLogin" class="list-inline mb-0">
+    <ul class="list-inline mb-0" v-if="!user.isLogin">
       <li class="list-inline-item">
-        <a href="#" class="btn btn-outline-light my-2">登录</a>
+        <a class="btn btn-outline-light my-2" href="#">登录</a>
       </li>
       <li class="list-inline-item">
-        <a href="#" class="btn btn-outline-light my-2">注册</a>
+        <a class="btn btn-outline-light my-2" href="#">注册</a>
       </li>
     </ul>
-    <ul v-else class="list-inline mb-0">
+    <ul class="list-inline mb-0" v-else>
       <li class="list-inline-item">
-        <a href="#" class="btn btn-outline-light my-2">你好 {{ user.name }}</a>
+        <a class="btn btn-outline-light my-2" href="#">你好 {{ user.name }}</a>
       </li>
     </ul>
   </nav>
