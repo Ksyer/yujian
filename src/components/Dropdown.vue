99a832f1ac6306768ff16492bc6b2ac90331fc6f
huatree <wang.yaohua@foxmail.com>
2021-05-09 16:47:06 +0800

    ✨ feat(顶部导航): 下拉按钮：编写下拉框组件

---
+++ src/components/Dropdown.vue
@@ -0,0 +1,60 @@
+<template>
+  <div class="dropdown" ref="dropdownRef">
+    <a
+      class="btn btn-outline-light my-2 dropdown-toggle"
+      href="#"
+      @click="toggleOpen"
+      >{{ title }}</a
+    >
+    <ul class="dropdown-menu" :style="{ display: 'block' }" v-if="isOpen">
+      <slot></slot>
+    </ul>
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent, ref, onMounted, onUnmounted } from 'vue'
+
+export default defineComponent({
+  name: 'Dropdown',
+  props: {
+    title: {
+      type: String,
+      require: true
+    }
+  },
+  setup() {
+    // 是否打开下拉框
+    const isOpen = ref(false)
+    const dropdownRef = ref<null | HTMLElement>(null)
+
+    /**
+     * 按钮：点击打开/关闭下拉框
+     */
+    const toggleOpen = () => {
+      isOpen.value = !isOpen.value
+    }
+
+    /**
+     * 鼠标事件：监听关闭下拉框
+     */
+    const handler = (e: MouseEvent) => {
+      if(dropdownRef.value) {
+        if(!dropdownRef.value.contains(e.target as HTMLElement) && isOpen) {
+          isOpen.value = false
+        }
+      }
+    }
+
+    onMounted(() => {
+      document.addEventListener('click', handler)
+    })
+
+    onUnmounted(() => {
+      document.removeEventListener('click', handler)
+    })
+
+    return { isOpen, toggleOpen, dropdownRef }
+  }
+})
+</script>
--- src/components/GlobalHeader.vue
+++ src/components/GlobalHeader.vue
@@ -11,7 +11,17 @@
     </ul>
     <ul class="list-inline mb-0" v-else>
       <li class="list-inline-item">
-        <a class="btn btn-outline-light my-2" href="#">你好 {{ user.name }}</a>
+        <dropdown :title="`你好${user.name}`">
+          <dropdown-item
+            ><a class="dropdown-item" href="#">新建文章</a></dropdown-item
+          >
+          <dropdown-item disabled
+            ><a class="dropdown-item" href="#">编辑资料</a></dropdown-item
+          >
+          <dropdown-item
+            ><a class="dropdown-item" href="#">退出登录</a></dropdown-item
+          >
+        </dropdown>
       </li>
     </ul>
   </nav>
@@ -19,6 +29,8 @@
 
 <script lang="ts">
 import { defineComponent, PropType } from 'vue'
+import Dropdown from './Dropdown.vue'
+import DropdownItem from './DropdownItem.vue'
 
 export interface UserProps {
   isLogin: boolean
@@ -28,6 +40,7 @@ export interface UserProps {
 
 export default defineComponent({
   name: 'GlobalHeader',
+  components: { Dropdown, DropdownItem },
   props: {
     user: {
       type: Object as PropType<UserProps>,
