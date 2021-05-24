8255f01cb9fa6b4547517350452f72f2b7da4b6c
huatree <wang.yaohua@foxmail.com>
2021-05-25 00:11:55 +0800

    ✨ feat(登录): 编写页面和业务逻辑

---
+++ src/views/Login.vue
@@ -0,0 +1,66 @@
+<template>
+  <validate-form @form-submit="onFormSubmit">
+    <div class="mb-3">
+      <label for="exampleInputEmail1" class="form-label">邮箱</label>
+      <validate-input
+        :rules="emailRules"
+        v-model="emailVal"
+        type="text"
+        placeholder="请输入邮箱"
+        ref="inputRef"
+      ></validate-input>
+    </div>
+    <div class="mb-3">
+      <label for="exampleInputEmail1" class="form-label">密码</label>
+      <validate-input
+        :rules="passwordRules"
+        v-model="passwordVal"
+        type="password"
+        placeholder="请输入密码"
+      ></validate-input>
+    </div>
+  </validate-form>
+</template>
+
+<script lang="ts">
+import { defineComponent, ref } from 'vue'
+import { useStore } from 'vuex'
+import { useRouter } from 'vue-router'
+
+import ValidateInput, { RulesProp } from '@/components/ValidateInput.vue'
+import ValidateForm from '@/components/ValidateForm.vue'
+
+export default defineComponent({
+  name: 'Login',
+  components: { ValidateInput, ValidateForm },
+  setup() {
+    const store = useStore()
+    const router = useRouter()
+    const emailVal = ref('')
+    const emailRules: RulesProp = [
+      { type: 'required', message: '电子邮箱地址不能为空' },
+      { type: 'email', message: '请输入正确的电子邮箱格式' }
+    ]
+    const passwordVal = ref('')
+    const passwordRules: RulesProp = [
+      { type: 'required', message: '密码不能为空' }
+    ]
+
+    const onFormSubmit = (result: boolean) => {
+      if (result) {
+        router.push('/')
+        store.commit('login')
+      }
+    }
+
+    return {
+      emailRules,
+      emailVal,
+      passwordVal,
+      passwordRules,
+      onFormSubmit
+    }
+  }
+})
+</script>
+
