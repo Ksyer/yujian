07f3a65945b50bd78954ed535ce9c92b66639d5a
huatree <wang.yaohua@foxmail.com>
2021-05-25 00:15:36 +0800

    ✨ feat(公共组件): 编写顶部组件路由跳转

--- src/components/GlobalHeader.vue
+++ src/components/GlobalHeader.vue
@@ -1,9 +1,11 @@
 <template>
   <nav class="navbar navbar-dark bg-primary justify-content-between mb-4 px-4">
-    <a class="navbar-brand" href="#">喻鉴专栏</a>
+    <router-link class="navbar-brand" :to="'/'">喻鉴专栏</router-link>
     <ul class="list-inline mb-0" v-if="!user.isLogin">
       <li class="list-inline-item">
-        <a class="btn btn-outline-light my-2" href="#">登录</a>
+        <router-link class="btn btn-outline-light my-2" :to="'/login'"
+          >登录</router-link
+        >
       </li>
       <li class="list-inline-item">
         <a class="btn btn-outline-light my-2" href="#">注册</a>
@@ -13,10 +15,9 @@
       <li class="list-inline-item">
         <dropdown :title="`你好${user.name}`">
           <dropdown-item
-            ><a class="dropdown-item" href="#">新建文章</a></dropdown-item
+            ><router-link class="dropdown-item" to="/create">新建文章</router-link></dropdown-item
           >
-          <dropdown-item disabled
-            ><a class="dropdown-item" href="#">编辑资料</a></dropdown-item
+          <dropdown-item disabled><a class="dropdown-item" href="#">编辑资料</a></dropdown-item
           >
           <dropdown-item
             ><a class="dropdown-item" href="#">退出登录</a></dropdown-item
@@ -31,12 +32,7 @@
 import { defineComponent, PropType } from 'vue'
 import Dropdown from './Dropdown.vue'
 import DropdownItem from './DropdownItem.vue'
-
-export interface UserProps {
-  isLogin: boolean
-  name?: string
-  id?: number
-}
+import { UserProps } from '@/store'
 
 export default defineComponent({
   name: 'GlobalHeader',
@@ -50,5 +46,3 @@ export default defineComponent({
 })
 </script>
 
-<style>
-</style>
