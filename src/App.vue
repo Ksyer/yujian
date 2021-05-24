9efd09839f015e3841c48fd0aff16966d75bf58c
huatree <wang.yaohua@foxmail.com>
2021-05-25 00:10:18 +0800

    ✨ feat(App.js): 添加页面主体和底部内容

--- src/App.vue
+++ src/App.vue
@@ -1,66 +1,44 @@
 <template>
   <div class="container">
+    <!-- 顶部 -->
     <global-header :user="currentUser"></global-header>
-    <column-list :list="list"></column-list>
+
+    <!-- 主体 -->
+    <router-view></router-view>
+    <!-- 底部 -->
+    <footer class="text-center py-4 text-secondary bg-light mt-6">
+      <small>
+        <ul class="list-inline- mb-0">
+          <li class="list-inline-item">2021 喻鉴专栏</li>
+          <li class="list-inline-item">文档</li>
+          <li class="list-inline-item">联系</li>
+          <li class="list-inline-item">更多</li>
+        </ul>
+      </small>
+    </footer>
   </div>
 </template>
 
 <script lang="ts">
-import { defineComponent } from 'vue'
-
-import GlobalHeader, { UserProps } from './components/GlobalHeader.vue'
-import ColumnList, { ColumnProps } from './components/ColumnList.vue'
+import { computed, defineComponent } from 'vue'
+import { useStore } from 'vuex'
+import GlobalHeader from './components/GlobalHeader.vue'
 
 import 'bootstrap/dist/css/bootstrap.min.css'
 
-const currentUser: UserProps = {
-  isLogin: true,
-  name: 'wyh',
-  id: 1
-}
-
-const testData: ColumnProps[] = [
-  {
-    id: 1,
-    title: 'test1的专栏',
-    description: '这是一个专栏，有一段非常有意思的简介，可以看看',
-    avatar:
-      'http://vue-maker.oss-cn-hangzhou.aliyuncs.com/vue-marker/5ee22dd58b3c4520912b9470.jpg?x-oss-process=image/resize,m_pad,h_100,w_100'
-  },
-  {
-    id: 2,
-    title: 'test2的专栏',
-    description:
-      '这是一个专栏，有一段非常有意思的简介，可以看看，走过路过不要错过',
-    avatar:
-      'http://vue-maker.oss-cn-hangzhou.aliyuncs.com/vue-marker/5ee22dd58b3c4520912b9470.jpg?x-oss-process=image/resize,m_pad,h_100,w_100'
-  },
-  {
-    id: 3,
-    title: 'test3的专栏',
-    description: '这是一个专栏，有一段非常有意思的简介，可以看看',
-    avatar:
-      'http://vue-maker.oss-cn-hangzhou.aliyuncs.com/vue-marker/5ee22dd58b3c4520912b9470.jpg?x-oss-process=image/resize,m_pad,h_100,w_100'
-  },
-  {
-    id: 4,
-    title: 'test4的专栏',
-    description: '这是一个专栏，有一段非常有意思的简介，可以看看'
-    // avatar: 'http://vue-maker.oss-cn-hangzhou.aliyuncs.com/vue-marker/5ee22dd58b3c4520912b9470.jpg?x-oss-process=image/resize,m_pad,h_100,w_100'
-  }
-]
-
 export default defineComponent({
   name: 'App',
-  components: { GlobalHeader, ColumnList },
+  components: {
+    GlobalHeader
+  },
   setup() {
+    const store = useStore()
+    const currentUser = computed(() => store.state.user)
+
     return {
-      currentUser,
-      list: testData
+      currentUser
     }
   }
 })
 </script>
 
-<style>
-</style>
