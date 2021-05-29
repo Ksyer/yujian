d8c2ed67e247aebddea30345ffb7ccf9c6437204
huatree <wang.yaohua@foxmail.com>
2021-05-29 15:12:03 +0800

    🎈 perf(store): 优化减少重复请求

--- src/store/index.ts
+++ src/store/index.ts
@@ -72,8 +72,8 @@ export interface GlobalDataProps {
   error: GlobalErrorProps
   token: string
   isLoading: boolean
-  columns: ListProps<ColumnProps>
-  posts: ListProps<PostProps>
+  columns: { data: ListProps<ColumnProps>; isLoaded: boolean }
+  posts: { data: ListProps<PostProps>; loadedColumns: string[] }
   user: UserProps
 }
 
@@ -82,22 +82,24 @@ const store = createStore<GlobalDataProps>({
     error: { status: false },
     token: localStorage.getItem('token') || '',
     isLoading: false,
-    columns: {},
-    posts: {},
+    columns: { data: {}, isLoaded: false },
+    posts: { data: {}, loadedColumns: [] },
     user: { isLogin: false }
   },
   mutations: {
     createPost(state, newPost) {
-      state.posts[newPost._id] = newPost
+      state.posts.data[newPost._id] = newPost
     },
     getColumns(state, rawData) {
-      state.columns = arrToObj(rawData.data.list)
+      state.columns.data = arrToObj(rawData.data.list)
+      state.columns.isLoaded = true
     },
     getColumn(state, rawData) {
-      state.columns[rawData.data._id] = rawData.data
+      state.columns.data[rawData.data._id] = rawData.data
     },
-    getPosts(state, rawData) {
-      state.posts = arrToObj(rawData.data.list)
+    getPosts(state, { data: rawData, extraData: columnId }) {
+      state.posts.data = { ...state.posts.data, ...arrToObj(rawData.data.list) }
+      state.posts.loadedColumns.push(columnId)
     },
     setLoading(state, status) {
       state.isLoading = status
@@ -121,27 +123,33 @@ const store = createStore<GlobalDataProps>({
       delete axios.defaults.headers.common.Authorization
     },
     getPost(state, rawData) {
-      state.posts[rawData.data._id] = rawData.data
+      state.posts.data[rawData.data._id] = rawData.data
     },
     updatePost(state, { data }) {
-      state.posts[data._id] = data
+      state.posts.data[data._id] = data
     },
     deletePost(state, { data }) {
-      delete state.posts[data._id]
+      delete state.posts.data[data._id]
     }
   },
   actions: {
-    async getColumns(ctx) {
-      const res = await reqColumns()
-      ctx.commit('getColumns', res.data)
+    async getColumns({ state, commit }) {
+      if (!state.columns.isLoaded) {
+        const res = await reqColumns()
+        commit('getColumns', res.data)
+      }
     },
-    async getColumn({ commit }, cid: string) {
-      const res = await reqColumn(cid)
-      commit('getColumn', res.data)
+    async getColumn({ state, commit }, cid: string) {
+      if (!state.columns.data[cid]) {
+        const res = await reqColumn(cid)
+        commit('getColumn', res.data)
+      }
     },
-    async getPosts({ commit }, cid: string) {
-      const res = await reqPosts(cid)
-      commit('getPosts', res.data)
+    async getPosts({ state, commit }, cid: string) {
+      if (!state.posts.loadedColumns.includes(cid)) {
+        const res = await reqPosts(cid)
+        commit('getPosts', { data: res.data, extraData: cid })
+      }
     },
     async login({ commit }, payload: LoginProps) {
       const res = await reqLogin(payload)
@@ -160,10 +168,16 @@ const store = createStore<GlobalDataProps>({
       const res = await reqCreatePost(payload)
       commit('createPost', res.data)
     },
-    async getPost({ commit }, id: string) {
-      const res = await reqPost(id)
-      commit('getPost', res.data)
-      return res.data
+    async getPost({ state, commit }, id: string) {
+      const currentPost = state.posts.data[id]
+
+      if (!currentPost || !currentPost.content) {
+        const res = await reqPost(id)
+        commit('getPost', res.data)
+        return res.data
+      } else {
+        return Promise.resolve({ data: currentPost })
+      }
     },
     async updatePost({ commit }, payload: PostProps) {
       const res = await reqUpdatePost(payload)
@@ -177,16 +191,16 @@ const store = createStore<GlobalDataProps>({
   },
   getters: {
     getColumns: state => {
-      return objToArr(state.columns)
+      return objToArr(state.columns.data)
     },
     getColumnById: state => (id: string) => {
-      return state.columns[id]
+      return state.columns.data[id]
     },
     getPostsById: state => (cid: string) => {
-      return objToArr(state.posts).filter(post => post.column === cid)
+      return objToArr(state.posts.data).filter(post => post.column === cid)
     },
     getCurrentPost: state => (id: string) => {
-      return state.posts[id]
+      return state.posts.data[id]
     }
   }
 })
