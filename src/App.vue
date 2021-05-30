34b6d141b404533599aebf52df8509444501086d
huatree <wang.yaohua@foxmail.com>
2021-05-30 15:38:35 +0800

    🌈 style(style): 修改页面样式

--- src/App.vue
+++ src/App.vue
@@ -1,23 +1,25 @@
 <template>
-  <div class="container-fluid px-0 flex-shrink-0">
-    <!-- 顶部 -->
-    <global-header :user="currentUser"></global-header>
-    <h1 v-if="error.message">{{ error.message }}</h1>
-    <loader v-if="isLoading"></loader>
-    <!-- 主体 -->
-    <router-view></router-view>
+  <div id="app" class="d-flex flex-column h-100">
+    <div class="container-fluid px-0 flex-shrink-0">
+      <!-- 顶部 -->
+      <global-header :user="currentUser"></global-header>
+      <h1 v-if="error.message">{{ error.message }}</h1>
+      <loader v-if="isLoading"></loader>
+      <!-- 主体 -->
+      <router-view></router-view>
+    </div>
+    <!-- 底部 -->
+    <footer class="text-center py-4 text-secondary bg-light mt-auto">
+      <small>
+        <ul class="list-inline- mb-0">
+          <li class="list-inline-item">2021 喻鉴专栏</li>
+          <li class="list-inline-item">文档</li>
+          <li class="list-inline-item">联系</li>
+          <li class="list-inline-item">更多</li>
+        </ul>
+      </small>
+    </footer>
   </div>
-  <!-- 底部 -->
-  <footer class="text-center py-4 text-secondary bg-light mt-6">
-    <small>
-      <ul class="list-inline- mb-0">
-        <li class="list-inline-item">2021 喻鉴专栏</li>
-        <li class="list-inline-item">文档</li>
-        <li class="list-inline-item">联系</li>
-        <li class="list-inline-item">更多</li>
-      </ul>
-    </small>
-  </footer>
 </template>
 
 <script lang="ts">
